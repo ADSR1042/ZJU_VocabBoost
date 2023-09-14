a20d565403c1fc9df0ce71b6eb8786d061aae83e
ADSR <ADSR1042@gmail.com>
2023-09-14 20:14:06 +0800

    update:仅当点击播放时音频加载

--- src/AudioPlayer.js
+++ src/AudioPlayer.js
@@ -1,33 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { NotificationOutlined } from "@ant-design/icons";
 
 export const AudioPlayer = ({ src }) => {
-  const [audio] = useState(new Audio());
-  const [isLoaded, setIsLoaded] = useState(false);
-
-  useEffect(() => {
-    // 预加载音频
-    audio.src = src;
-    audio.load();
-    audio.addEventListener("canplaythrough", () => {
-      setIsLoaded(true);
-    });
-
-    // 在组件卸载时清除事件监听和音频对象
-    return () => {
-      audio.removeEventListener("canplaythrough", () => {
-        setIsLoaded(true);
-      });
-      audio.pause();
-      audio.src = "";
-      audio.load();
-    };
-  }, [src]);
+  const [audio, setAudio] = useState(null);
 
   const play = () => {
-    if (isLoaded) {
-      audio.play().catch((err) => {
+    if (!audio||audio.src !== src) {
+      const newAudio = new Audio(src);
+      newAudio.play().catch((err) => {
         console.log(err);
+      });
+      setAudio(newAudio);
+    } else {
+      audio.play().catch((err) => {
+        // console.log(err);
       });
     }
   };
